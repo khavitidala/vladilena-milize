0cdcb92616027533385b37066ba4cb161bda8a44
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2021-11-12 08:25:04 +0700

    add utis

---
+++ multi-class-classification/utils.py
@@ -0,0 +1,253 @@
+import os
+import shutil
+from copy import deepcopy
+import random
+import numpy as np
+import pandas as pd
+import torch
+import string
+import re
+import torch.nn.functional as F
+
+from torch import optim
+from torch.optim.lr_scheduler import StepLR
+from tqdm import tqdm
+from transformers import AdamW, BertForSequenceClassification, BertConfig, BertTokenizer, AutoTokenizer, AlbertForSequenceClassification
+from nltk.tokenize import TweetTokenizer
+
+from torch.utils.data import Dataset, DataLoader 
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+
+def document_sentiment_metrics_fn(list_hyp, list_label):
+    metrics = {}
+    metrics["ACC"] = accuracy_score(list_label, list_hyp)
+    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
+    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
+    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    return metrics
+
+# Forward function for sequence classification
+def forward_sequence_classification(model, batch_data, i2w, is_test=False, device='cpu', **kwargs):
+    # Unpack batch data
+    if len(batch_data) == 3:
+        (subword_batch, mask_batch, label_batch) = batch_data
+        token_type_batch = None
+    elif len(batch_data) == 4:
+        (subword_batch, mask_batch, token_type_batch, label_batch) = batch_data
+
+    # Prepare input & label
+    subword_batch = torch.LongTensor(subword_batch)
+    mask_batch = torch.FloatTensor(mask_batch)
+    token_type_batch = torch.LongTensor(token_type_batch) if token_type_batch is not None else None
+    label_batch = torch.LongTensor(label_batch)
+
+    if device == "cuda":
+        subword_batch = subword_batch.cuda()
+        mask_batch = mask_batch.cuda()
+        token_type_batch = token_type_batch.cuda() if token_type_batch is not None else None
+        label_batch = label_batch.cuda()
+
+    # Forward model
+    outputs = model(subword_batch, attention_mask=mask_batch, token_type_ids=token_type_batch, labels=label_batch)
+    loss, logits = outputs[:2]
+
+    # generate prediction & label list
+    list_hyp = []
+    list_label = []
+    hyp = torch.topk(logits, 1)[1]
+    for j in range(len(hyp)):
+        list_hyp.append(i2w[hyp[j].item()])
+        list_label.append(i2w[label_batch[j][0].item()])
+
+    return loss, list_hyp, list_label
+
+class DocumentSentimentDataset(Dataset):
+    # Static constant variable
+    LABEL2INDEX = i2w #{'positive': 0, 'neutral': 1, 'negative': 2}
+    INDEX2LABEL = w2i #{0: 'positive', 1: 'neutral', 2: 'negative'}
+    NUM_LABELS = args["num_labels"] #3
+
+    def load_dataset(self, path):
+        df = pd.read_csv(path, sep='\t', header=None)
+        df.columns = ['text','sentiment']
+        df['sentiment'] = df['sentiment'].apply(lambda lab: w2i[lab])
+        return df
+
+    def __init__(self, dataset_path, tokenizer, no_special_token=False, *args, **kwargs):
+        self.data = self.load_dataset(dataset_path)
+        self.tokenizer = tokenizer
+        self.no_special_token = no_special_token
+
+    def __getitem__(self, index):
+        data = self.data.loc[index,:]
+        text, sentiment = data['text'], data['sentiment']
+        subwords = self.tokenizer.encode(text, add_special_tokens=not self.no_special_token)
+        return np.array(subwords), np.array(sentiment), data['text']
+
+    def __len__(self):
+        return len(self.data)
+
+class DocumentSentimentDataLoader(DataLoader):
+    def __init__(self, max_seq_len=512, *args, **kwargs):
+        super(DocumentSentimentDataLoader, self).__init__(*args, **kwargs)
+        self.collate_fn = self._collate_fn
+        self.max_seq_len = max_seq_len
+
+    def _collate_fn(self, batch):
+        batch_size = len(batch)
+        max_seq_len = max(map(lambda x: len(x[0]), batch))
+        max_seq_len = min(self.max_seq_len, max_seq_len)
+
+        subword_batch = np.zeros((batch_size, max_seq_len), dtype=np.int64)
+        mask_batch = np.zeros((batch_size, max_seq_len), dtype=np.float32)
+        sentiment_batch = np.zeros((batch_size, 1), dtype=np.int64)
+
+        seq_list = []
+        for i, (subwords, sentiment, raw_seq) in enumerate(batch):
+            subwords = subwords[:max_seq_len]
+            subword_batch[i,:len(subwords)] = subwords
+            mask_batch[i,:len(subwords)] = 1
+            sentiment_batch[i,0] = sentiment
+
+            seq_list.append(raw_seq)
+
+        return subword_batch, mask_batch, sentiment_batch, seq_list
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+###
+# modelling functions
+###
+def get_lr(args, optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+def metrics_to_string(metric_dict):
+    string_list = []
+    for key, value in metric_dict.items():
+        string_list.append('{}:{:.2f}'.format(key, value))
+    return ' '.join(string_list)
+
+###
+# Training & Evaluation Function
+###
+
+# Evaluate function for validation and test
+def evaluate(model, data_loader, forward_fn, metrics_fn, i2w, is_test=False):
+    model.eval()
+    total_loss, total_correct, total_labels = 0, 0, 0
+
+    list_hyp, list_label, list_seq = [], [], []
+
+    pbar = tqdm(iter(data_loader), leave=True, total=len(data_loader))
+    for i, batch_data in enumerate(pbar):
+        batch_seq = batch_data[-1]
+        loss, batch_hyp, batch_label = forward_fn(model, batch_data[:-1], i2w=i2w, device=args['device'])
+
+
+        # Calculate total loss
+        test_loss = loss.item()
+        total_loss = total_loss + test_loss
+
+        # Calculate evaluation metrics
+        list_hyp += batch_hyp
+        list_label += batch_label
+        list_seq += batch_seq
+        metrics = metrics_fn(list_hyp, list_label)
+
+        if not is_test:
+            pbar.set_description("VALID LOSS:{:.4f} {}".format(total_loss/(i+1), metrics_to_string(metrics)))
+        else:
+            pbar.set_description("TEST LOSS:{:.4f} {}".format(total_loss/(i+1), metrics_to_string(metrics)))
+
+    if is_test:
+        return total_loss, metrics, list_hyp, list_label, list_seq
+    else:
+        return total_loss, metrics
+
+# Training function and trainer
+def train(model, train_loader, valid_loader, optimizer, forward_fn, metrics_fn, valid_criterion, i2w, n_epochs, evaluate_every=1, early_stop=3, step_size=1, gamma=0.5, model_dir="", exp_id=None):
+    scheduler = StepLR(optimizer, step_size=step_size, gamma=gamma)
+
+    best_val_metric = -100
+    count_stop = 0
+
+    for epoch in range(n_epochs):
+        model.train()
+        total_train_loss = 0
+        list_hyp, list_label = [], []
+
+        train_pbar = tqdm(iter(train_loader), leave=True, total=len(train_loader))
+        for i, batch_data in enumerate(train_pbar):
+            loss, batch_hyp, batch_label = forward_fn(model, batch_data[:-1], i2w=i2w, device=args['device'])
+
+            optimizer.zero_grad()
+            if args['fp16']:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+                torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args['max_norm'])
+            else:
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args['max_norm'])
+            optimizer.step()
+
+            tr_loss = loss.item()
+            total_train_loss = total_train_loss + tr_loss
+
+            # Calculate metrics
+            list_hyp += batch_hyp
+            list_label += batch_label
+
+            train_pbar.set_description("(Epoch {}) TRAIN LOSS:{:.4f} LR:{:.8f}".format((epoch+1),
+                total_train_loss/(i+1), get_lr(args, optimizer)))
+
+        metrics = metrics_fn(list_hyp, list_label)
+        print("(Epoch {}) TRAIN LOSS:{:.4f} {} LR:{:.8f}".format((epoch+1),
+            total_train_loss/(i+1), metrics_to_string(metrics), get_lr(args, optimizer)))
+
+        # Decay Learning Rate
+        scheduler.step()
+
+        # evaluate
+        if ((epoch+1) % evaluate_every) == 0:
+            val_loss, val_metrics = evaluate(model, valid_loader, forward_fn, metrics_fn, i2w, is_test=False)
+
+            # Early stopping
+            val_metric = val_metrics[valid_criterion]
+            if best_val_metric < val_metric:
+                best_val_metric = val_metric
+                # save model
+                if exp_id is not None:
+                    torch.save(model.state_dict(), model_dir + "/best_model_" + str(exp_id) + ".th")
+                else:
+                    torch.save(model.state_dict(), model_dir + "/best_model.th")
+                count_stop = 0
+            else:
+                count_stop += 1
+                print("count stop:", count_stop)
+                if count_stop == early_stop:
+                    break
+
+def load_model(args):
+  if 'indobenchmark' in args['model_checkpoint']:
+    # indobenchmark models
+    # Prepare config & tokenizer
+    vocab_path, config_path = None, None
+    tokenizer = BertTokenizer.from_pretrained(args['model_checkpoint'])
+    config = BertConfig.from_pretrained(args['model_checkpoint'])
+    if type(args['num_labels']) == list:
+        config.num_labels = max(args['num_labels'])
+        config.num_labels_list = args['num_labels']
+    else:
+        config.num_labels = args['num_labels']
+
+    # Instantiate model
+    model_class = None
+    if 'sequence_classification' == args['task']:
+        model_class = AlbertForSequenceClassification if 'lite' in args['model_checkpoint'] else BertForSequenceClassification
+    model = model_class.from_pretrained(args['model_checkpoint'], config=config)
+  return model, tokenizer, vocab_path, config_path
