999fcf10b9c7a5123cf90fc847bdac519f5f4860
Ryan Abdurohman <54085090+khavitidala@users.noreply.github.com>
2021-11-12 09:35:17 +0700

    update utils

--- multi-class-classification/utils.py
+++ multi-class-classification/utils.py
@@ -69,7 +69,7 @@ class DocumentMultiClassDataset(Dataset):
         df['label'] = df['label'].apply(lambda lab: w2i[lab])
         return df
 
-    def __init__(self, dataset_df, tokenizer, no_special_token=False, w2i, *args, **kwargs):
+    def __init__(self, dataset_df, tokenizer, w2i, no_special_token=False, *args, **kwargs):
         self.data = self.load_dataset(dataset_df, w2i)
         self.tokenizer = tokenizer
         self.no_special_token = no_special_token
